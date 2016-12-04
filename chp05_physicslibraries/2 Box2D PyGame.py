6070eaead02369cbe6b0eb7997045199e943df26
Michail Koroteev <sairos@ro.ru>
2016-12-04 17:22:16 +0300

    Chapter 4 refactored using multiple files

--- chp05_physicslibraries/2 Box2D PyGame.py
+++ chp05_physicslibraries/2 Box2D PyGame.py
@@ -2,121 +2,22 @@
 
 
 # region imports
-import random
-import numpy as np
-import pygame
-from numpy import array as vector
-from math import pi
 
-from Box2D import b2PolygonShape, b2World, b2CircleShape
+import pygame
+from Box2D import b2World
+
+from chp05_physicslibraries.Globals import WIDTH, HEIGHT, WHITE, mousepos, is_mouse_down, is_rmouse_down
+from chp05_physicslibraries.ParticleSystem import ParticleSystem
 
 # endregion
 
 
 # region globals
 
-WIDTH = 800
-HEIGHT = 600
-
-BLACK = (0, 0, 0)
-WHITE = (255, 255, 255)
-GREEN = (0, 255, 0)
-RED = (255, 0, 0)
-
-mousepos = vector([WIDTH / 2, HEIGHT / 2])
-is_mouse_down = False
-is_rmouse_down = False
-
-scale_factor = 10.0
-transX = WIDTH / 2
-transY = HEIGHT / 2
-
-
-def vec_pixels2world(vec2):
-    x, y = vec2[0], vec2[1]
-    x_ = (x - transX) / scale_factor
-    y_ = (transY - y) / scale_factor
-    return x_, y_
-
-
-def vec_world2pixels(vec2):
-    x, y = vec2[0], vec2[1]
-    x_ = x * scale_factor + transX
-    y_ = (1 - y) * scale_factor + transY
-    return x_, y_
-
-
-def scalar_pixels2world(val):
-    return val / scale_factor
-
-
-def scalar_world2pixels(val):
-    return val * scale_factor
-
-
 # endregion
 
 
 # region class definition
-
-class Particle:
-    def __init__(self, world, pos, size=None):
-        pos = vec_pixels2world(pos)
-        if size:
-            self.size = size
-        else:
-            self.size = (random.random() * 10 + 5, random.random() * 10 + 5)
-
-        shape = b2PolygonShape(box=(scalar_pixels2world(self.size[0] / 2),
-                                    scalar_pixels2world(self.size[1] / 2)))
-        self.body = world.CreateDynamicBody(position=(pos[0], pos[1]),
-                                            shapes=shape)
-        self.box = self.body.CreateCircleFixture(shape=shape, density=1.0, friction=0.3)
-
-        self.lifespan = 150
-        self.life = self.lifespan
-
-    def draw(self, scr):
-        s = pygame.Surface(self.size, pygame.SRCALPHA)  # per-pixel alpha
-        s.fill((127, 127, 127, int((255.0 / self.lifespan) * self.life)))  # notice the alpha value in the color
-        pygame.draw.rect(s, (0, 0, 0, int((255.0 / self.lifespan) * self.life)),
-                         [0, 0,
-                          self.size[0], self.size[1]], 3)
-        s = pygame.transform.rotate(s, self.body.angle * 180 / pi)
-
-        pos = vec_world2pixels(self.body.position)
-        pos = (pos[0] - self.size[0] / 2, pos[1] - self.size[1] / 2)
-        scr.blit(s, pos)
-
-    def delete(self, world):
-        world.DestroyBody(self.body)
-        pass
-
-
-class ParticleSystem:
-    def __init__(self, world, pos):
-        self.pos = pos
-        self.world = world
-        self.particles = []
-
-    def draw(self, c):
-        for particle in self.particles:
-            particle.draw(c)
-
-    def update(self):
-        if random.random() < 0.5:
-            self.particles.append(Particle(self.world, self.pos.copy()))
-
-        for particle in self.particles:
-            particle.life -= 1
-            if particle.life <= 0:
-                particle.delete(self.world)
-                self.particles.remove(particle)
-
-    def run(self, c):
-        self.update()
-        self.draw(c)
-
 
 # endregion
 
